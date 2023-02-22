6b1d5e7d35648cdbb75e763aa022ff1c4fcbf859
mohit-04 <mohitrana04032003@gmail.com>
2023-02-22 18:09:20 +0530

    updated css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { FaCalendar, FaBiPhoneCall, FaPhoneAlt } from "react-icons/fa";
+import classes from "./Card.module.css";
 import "../index.css";
 // https://jsonplaceholder.typicode.com/posts?_limit=8
 
@@ -29,44 +30,44 @@ const Card = () => {
         {users.map((ele) => {
           return (
             <>
-              <div class="flex-container">
-                <div class="flex-child first">
+              <div class={classes.flexContainer}>
+                <div class={classes.flexChild}>
                   {ele.userId}
                   <img
                     src="https://picsum.photos/seed/picsum/200/301"
                     height="50rem"
                     alt=""
-                    className="pfp"
+                    className={classes.pfp}
                   />{" "}
                   {/* how */}
-                  <h4 className="hospital-title">{ele.title}</h4>
+                  <h4 className={classes.hospitalTitle}>{ele.title}</h4>
                 </div>
 
-                <div class="flex-child1 second">
-                  <div className="experience">
-                    <p className="hos-name"> {ele.id} </p>
-                    <p className="exp-class">4 years</p>
+                <div class={classes.flexchild1}>
+                  <div className={classes.experience}>
+                    <p className={classes.hosName}> {ele.id} </p>
+                    <p className={classes.expClass}>4 years</p>
                   </div>
                   <p> 22-B bakers street </p>
                   <div
                     style={{ display: "flex", justifyContent: "space-around" }}
                   >
-                    <button className="button-11" role="button">
+                    <button className={classes.button11} role="button">
                       Back Pain
                     </button>
-                    <button className="button-11" role="button">
+                    <button className={classes.button11} role="button">
                       Foot
                     </button>
-                    <button className="button-11" role="button">
+                    <button className={classes.button11} role="button">
                       Cancer
                     </button>
-                    <button className="button-11" role="button">
+                    <button className={classes.button11} role="button">
                       Migraine
                     </button>
                   </div>
                   {/* https://icons8.com/icon/96298/call-male */}
                   <p>{ele.body}</p>
-                  <button className="button-3 to-right " role="button">
+                  <button className={classes.button3} role="button">
                     Call Now <FaPhoneAlt />
                   </button>
                 </div>
